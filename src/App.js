be96f7544227fc3a0ccf740369d80913b2a62ebd
marwaAhmedGalal <marwa2ahmed@gmail.com>
2023-08-28 12:03:07 +0300

    resolve the error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Counter from "./components/counter/counter";
 import Navbar from "./components/navebar/navBar";
 import { Provider } from "react-redux";
-import store from "./store/reucers/counterreducer";
+import store from "./store/reucers/navBarReducer";
 function App() {
   return (
     <div className="App">
